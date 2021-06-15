ee3a5eb427969a1d2fb3a502fbe4fbe42d7e30b1
Pavel Mosk <64908466+Pavlik07@users.noreply.github.com>
2021-06-15 12:51:07 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <span class="two_pics">
+      <Hero v-model="fightData" />
+      <Enemy v-model="fightData" />
+    </span>
+    <h1 v-if="endTurn">End of turn. Enemy hits you</h1>
+  </div>
+</template>
+
+<script>
+import Hero from './components/Hero'
+import Enemy from './components/Enemy'
+
+export default {
+  name: 'App',
+  components: {
+    Hero, Enemy
+  },
+  data() {
+    return {
+      fightData:{
+        heroHealthCounter: 100,
+        heroManaCounter: 200,
+        heroActionPoints: 3,
+        enemyHealthCounter: 200
+      }
+    }
+  },
+  watch: {
+    endTurn(val=this.heroHealthCounter) {
+      val.$nextTick() < val
+    }
+  }
+}
+</script>
+
+<style scoped>
+.two_pics{
+  display: flex;
+  justify-content: space-between;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+</style>
